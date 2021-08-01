05a0f654d44140fc699bfe26c15fd0b0dc9b5a59
mo <462894334@qq.com>
2021-08-01 22:36:15 +0800

    学习至第五集，初步了解了jsx的基本使用和一些注意事项

--- my-first-react-app/src/index.js
+++ my-first-react-app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './static/less/index.less'
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -15,3 +16,37 @@ import ReactDOM from 'react-dom';
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+/**
+ * 将jsx语法转换为真实dom，结构ReactDom.render([jsx], [container], [callback])
+ * @params jsx: jsx syntax
+ * @params container: 容器，我们想把元素放到页面中的哪个容器中
+ * @params callback: （不常用）当把内容放到页面中后呈现触发的回调函数
+ */ 
+const author = 'mo';
+const data = [{name: '张三', age: 18}, {name: '李四', age: 20}];
+const root = document.querySelector('#root')
+// ReactDOM.render(
+//   <div>hello world by {author}
+//   <ul>
+//     {
+//       data.map((item, index) => {
+//         const {name, age} = item;
+//         return  <li key={index}>
+//                   <span>{name}</span>
+//                   <span>{age}</span>
+//                 </li>;
+//       })
+//     }
+//   </ul>
+//   </div>, root)
+
+/** jsx中的用法
+ * 只能有一个父div，与vue的template一样
+ * className 替代 class
+ * style 里面{}再包含了一个对象 即{{}}
+ */
+ReactDOM.render(<div id="box" className="box">
+  <h1>{author}</h1>
+  <span style={{fontSize: '30px', fontWeight: 'bold'}}>2021/8/1</span>
+</div>,root)
